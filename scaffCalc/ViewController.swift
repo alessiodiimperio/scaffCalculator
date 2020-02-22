1b51edade03322191939f091a00a647f06b4a1e4
Alessio Di Imperio <alessio.diimperio@gmail.com>
2020-02-22 15:12:32 +0100

    Initial Commit

---
+++ scaffCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scaffCalc
+//
+//  Created by Alessio on 2020-02-22.
+//  Copyright © 2020 Alessio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
